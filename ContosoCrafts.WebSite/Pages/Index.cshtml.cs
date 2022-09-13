f2c27c7c130f4ce908acf7cd257fbd99e4741b8c
CaioProg <caioprog18@gmail.com>
2022-09-13 09:08:35 -0300

    Data in a Razor Page | ASP.NET Core 101 [5 of 13]

--- ContosoCrafts.WebSite/Pages/Index.cshtml.cs
+++ ContosoCrafts.WebSite/Pages/Index.cshtml.cs
@@ -5,21 +5,28 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContosoCrafts.WebSite.Services;
+using ContosoCrafts.WebSite.Models;
 
 namespace ContosoCrafts.WebSite.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService ProductService;
+        public IEnumerable<Product> Products { get; private set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(
+            ILogger<IndexModel> logger,
+            JsonFileProductService productService)
         {
             _logger = logger;
+            ProductService = productService;
         }
 
         public void OnGet()
         {
-
+            Products = ProductService.GetProducts();
         }
     }
 }
