9efe76546b969530381b80edf965490ba922b8c4
CaioProg <caioprog18@gmail.com>
2022-10-16 15:32:03 -0300

    Enhancing your Web API: Part 2 | ASP.NET Core 101[9 of 13]

--- ContosoCrafts.WebSite/Controllers/ProductsController.cs
+++ ContosoCrafts.WebSite/Controllers/ProductsController.cs
@@ -23,5 +23,16 @@ namespace ContosoCrafts.WebSite.Controllers
             return ProductService.GetProducts();
         }
 
+        //[HttpPath] "[FromBody]"
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get(
+            [FromQuery] string ProductId, 
+            [FromQuery] int Rating)
+        {
+            ProductService.AddRating(ProductId, Rating);
+            return Ok();
+        }
+
     }
 }
